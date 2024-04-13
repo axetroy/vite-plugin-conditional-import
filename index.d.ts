ce41098fc08014bf379382477703afd704a519e7
Axetroy <axetroy.dev@gmail.com>
2024-04-13 10:31:34 +0800

    refactor: update typing

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,15 @@
 import { Plugin } from "vite";
 
 export interface ViteConditionalImportPluginOptions {
+  /**
+   * Define the current running environment.
+   * @example 'client'
+   */
   currentEnv: string | string[];
+  /**
+   * Define how your code can run in several environments.
+   * @example ['client', 'server']
+   */
   envs: string[];
 }
 
